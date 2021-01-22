ff5ea37d544f94c8fd18ec27f433a471486b5c09
achref mekni <chrefmekni1@hotmail.fr>
2021-01-22 12:30:28 +0100

    I Added ErrorBoundary class which is a new feature in React 16+

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../Components/CardList';
 import Scroll from '../Components/Scroll';
 import SearchBox from '../Components/SearchBox';
 import './App.css';
+import ErrorBoundry from '../Components/ErrorBoundry';
 
 
 class App extends Component {
@@ -40,7 +41,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
+                    <ErrorBoundry>
                 <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
                 </Scroll>
             </div>
         );
